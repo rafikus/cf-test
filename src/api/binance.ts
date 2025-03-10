d94e26b9eaeb9a3efc70de79b88ff17a37aa3b8f
rafikus <rafikus95@gmail.com>
2025-03-10 11:31:50 +0100

    add binance api query

---
+++ src/api/binance.ts
@@ -0,0 +1,13 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const binanceAPI = createApi({
+  reducerPath: "binanceApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://api.binance.com/api/v3" }),
+  endpoints: (builder) => ({
+    getKlinesForSymbol: builder.query({
+      query: (symbol) => `/klines?symbol=${symbol}&interval=1w`,
+    }),
+  }),
+});
+
+export const { useGetKlinesForSymbolQuery } = binanceAPI;
